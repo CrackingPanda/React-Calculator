afc3d0ef78bb1a176a2ff2fa1feee40de8486347
CrackingPanda <worlderlove@gmail.com>
2021-12-08 23:09:28 +0530

    Final coding and design commit

--- Calc.js
+++ Calc.js
@@ -7,10 +7,11 @@ class Keypad extends React.Component {
     }
     render() {
         return (
-            <button id={this.props.type} onClick={this.buttonClick.bind(this)}>{this.props.buttonKey}</button>
-        );
+            React.createElement("button", { id: this.props.type, onClick: this.buttonClick.bind(this) }, this.props.buttonKey));
+
     }
 }
+
 
 class Screen extends React.Component {
     constructor() {
@@ -19,41 +20,44 @@ class Screen extends React.Component {
     displayNumber2() {
         let number2 = this.props.num2;
         if (number2 !== 0) {
-            return number2
+            return number2;
         }
     }
     render() {
         return (
-            <div id="Screen">
-                <p>{this.props.input}</p>
-                <p className='text-small'><span>{this.displayNumber2()}</span><span>{this.props.mode}</span></p>
-            </div>
-        );
+            React.createElement("div", { id: "Screen" }, 
+                React.createElement("p", null, this.props.input), 
+                React.createElement("p", { className: "text-small" }, React.createElement("span", null, this.displayNumber2()), React.createElement("span", null, this.props.mode))));
+
+
     }
 }
+
 
 class Main extends React.Component {
     constructor() {
         super();
         this.state = {
-            number: "0", // number to display on top part of screen
+            number: "0", // number to display on screen
             mode: '', // math task (add, substract etc)
-            finished: false, // when finished, clear display screen
+            finished: false, // when finished, clear screen
             math: "",
-            dot: false,
-        }
+            dot: false
+        };
+
     }
 
-    //cleaning display screen
-    displayClear() { // reseting state to default state
+    //clearing screen
+    displayClear() {// reseting state to default state
         this.setState({
             number: "0",
             mode: '',
             finished: false,
             changingMode: false,
             math: "",
-            dot: false,
+            dot: false
         });
+
     }
 
     // Displaying numbers on screen
@@ -62,19 +66,22 @@ class Main extends React.Component {
         if (!this.state.finished) {
             if (this.state.number == "0") {
                 this.setState({
-                    number: pressedButton,
+                    number: pressedButton
                 });
+
             } else {
                 let number = this.state.number + pressedButton;
                 this.setState({
-                    number,
+                    number
                 });
+
             }
         } else
             this.setState({
                 number: pressedButton,
                 finished: false
             });
+
     }
 
     //calculating final number and returning it
@@ -86,25 +93,26 @@ class Main extends React.Component {
         return eval(math); //return finished math
     }
 
-    mathMode(e) { // picking up math mode + displaying current numbers
+    mathMode(e) {// picking up math mode + displaying current numbers
         let mode = e.target.innerHTML; //picking math option
         let number = this.state.number; // current number
         let math = this.state.math; // current state of our mathematic formula
 
-        if (number == "0") { // if displaying number is 0, we can change math symbols
-            if (math[math.length - 1] !== mode) { // check if different symbol was pressed, if yes, swap it
+        if (number == "0") {// if displaying number is 0, we can change math symbols
+            if (math[math.length - 1] !== mode) {// check if different symbol was pressed, if yes, swap it
                 math = math.substring(0, math.length - 1) + mode; // < in here
             }
             this.setState({ //save current mode and math formula
                 mode: "",
-                math, //math: math
+                math //math: math
             });
-        } else { //if we press math symbol when we have already pressed number, treat it as adding new piece of a math chain, so add it
+        } else {//if we press math symbol when we have already pressed number, treat it as adding new piece of a math chain, so add it
             this.setState({
                 math: math + number + mode, // if we clicked another number, save formula and reset number and mode
                 number: "0",
-                mode: "",
+                mode: ""
             });
+
         }
     }
 
@@ -114,8 +122,9 @@ class Main extends React.Component {
             number += ".";
             this.setState({
                 dot: true,
-                number: number,
+                number: number
             });
+
         }
     }
 
@@ -127,35 +136,37 @@ class Main extends React.Component {
                 number,
                 finished: true,
                 mode: "",
-                dot: false,
+                dot: false
             });
+
         }
     }
 
     render() {
         return (
-            <div className='center-block' id='Main'>
-                <h1>React Calculator</h1>
-                <Screen input={this.state.number} num2={this.state.math} mode={this.state.mode} />
-                <Keypad type='clear' clickHandle={this.displayClear.bind(this)} buttonKey={'Clear'} />
-                {
-                    ['+', '-', '÷', '*'].map((val, i) => {
-                        return <Keypad type={'math'} clickHandle={this.mathMode.bind(this)} key={i * 3} buttonKey={val} />
-                    })
-                }
-                <div id="Keypad">
-                    {
-                        [1, 2, 3, 4, 5, 6, 7, 8, 9].map((val, i) => {
-                            return <Keypad clickHandle={this.buttonClick.bind(this)} key={i} buttonKey={val} />
-                        })
-                    }
-                    <Keypad clickHandle={this.buttonClick.bind(this)} buttonKey={0} />
-                    <Keypad clickHandle={this.dotButtonClick.bind(this)} buttonKey={"."} />
-                    <Keypad type='result' clickHandle={this.mathResult.bind(this)} buttonKey={'='} />
-                </div>
-            </div>
-        );
+            React.createElement("div", { className: "center-block", id: "Main" }, 
+                React.createElement("h1", null, "React Calculator"), 
+                React.createElement(Screen, { input: this.state.number, num2: this.state.math, mode: this.state.mode }), 
+                React.createElement(Keypad, { type: "clear", clickHandle: this.displayClear.bind(this), buttonKey: 'Clear' }),
+
+                ['+', '-', '\u00F7', '*'].map((val, i) => {
+                    return React.createElement(Keypad, { type: 'math', clickHandle: this.mathMode.bind(this), key: i * 3, buttonKey: val });
+                }), 
+
+                React.createElement("div", { id: "Keypad" },
+
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9].map((val, i) => {
+                        return React.createElement(Keypad, { clickHandle: this.buttonClick.bind(this), key: i, buttonKey: val });
+                    }), 
+
+                    React.createElement(Keypad, { clickHandle: this.buttonClick.bind(this), buttonKey: 0 }), 
+                    React.createElement(Keypad, { clickHandle: this.dotButtonClick.bind(this), buttonKey: "." }), 
+                    React.createElement(Keypad, { type: "result", clickHandle: this.mathResult.bind(this), buttonKey: '=' }))));
+
+
+
     }
 }
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+
+ReactDOM.render( React.createElement(Main, null), document.getElementById('app'));
